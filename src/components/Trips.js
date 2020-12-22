b088be5ef4fa02e4678a61e85b7d5ae79854b45c
Eyyup <huakq@protonmail.com>
2020-12-23 00:32:14 +0100

    pull

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -4,21 +4,27 @@ import styled from "styled-components"
 export const Trips = () => {
   return (
     <div>
-      <ProductsContainer>
+      {/* <ProductsContainer>
         <ProductsHeading>Heading</ProductsHeading>
         <ProductWrapper>Wrapper</ProductWrapper>
-      </ProductsContainer>
+      </ProductsContainer> */}
+      <h1>test</h1>
     </div>
   )
 }
 
-const ProductsContainer = styled.div`
-  min-height: 100vh;
-  padding: 5rem calc((100vw - 1300px) / 2);
-  background: red;
-  color:#fff;
-`
+// const ProductsContainer = styled.div`
+//   min-height: 100vh;
+//   padding: 5rem calc((100vw - 1300px) / 2);
+//   background: red;
+//   color:#fff;
+// `
 
-const ProductsHeading = styled.div``
+// const ProductsHeading = styled.div`
+//   font-size: clamp(1.2rem, 5vw, 3rem);
+//   text-align: center;
+//   margin-bottom: 5rem;
+//   color: #000;
+// `
 
-const ProductWrapper = styled.div``
+// const ProductWrapper = styled.div``
