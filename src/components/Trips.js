750be475924fed579600855a843b3d032e3b26bb
Eyyup <huakq@protonmail.com>
2020-12-22 23:03:16 +0100

    trips changes

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react"
+import styled from "styled-components"
 
 export const Trips = () => {
-    return (
-        <div>
-            <h1>Trips</h1>
-        </div>
-    )
+  return (
+    <div>
+      <ProductsContainer>
+        <ProductsHeading>Heading</ProductsHeading>
+        <ProductWrapper>Wrapper</ProductWrapper>
+      </ProductsContainer>
+    </div>
+  )
 }
+
+const ProductsContainer = styled.div`
+  min-height: 100vh;
+  padding: 5rem calc((100vw - 1300px) / 2);
+  background: red;
+  color:#fff;
+`
+
+const ProductsHeading = styled.div``
+
+const ProductWrapper = styled.div``
